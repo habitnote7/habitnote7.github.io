a4275f4086027dd50cb994ff2c2195251af748fa
IrfanTheDev <theirfan2020@gmail.com>
2025-07-31 13:05:40 +0530

    Initial commit from child repo

---
+++ assets/ChangePassword.ec15fc7f.js
@@ -0,0 +1,1 @@
+import{aI as e,C as a,D as s,E as o}from"./index.be3bd688.js";const n={},r=o("h1",null,"Change Password Page",-1),t=[r];function c(_,d){return a(),s("div",null,t)}var f=e(n,[["render",c]]);export{f as default};
---
+++ assets/use-quasar.8d2f2e0f.js
@@ -0,0 +1,1 @@
+import{i as a,aB as r}from"./index.be3bd688.js";function u(){return a(r)}export{u};
